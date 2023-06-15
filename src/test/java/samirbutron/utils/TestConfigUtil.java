db422c91467749971fe1ab282f62772f9491ff39
Samir Butron <samirbut@hotmai.com>
2023-06-15 15:43:26 -0600

    REST_API task completed (after feedback)

---
+++ src/test/java/samirbutron/utils/TestConfigUtil.java
@@ -0,0 +1,18 @@
+package samirbutron.utils;
+
+import aquality.selenium.core.utilities.ISettingsFile;
+import aquality.selenium.core.utilities.JsonSettingsFile;
+
+public class TestConfigUtil {
+
+  private static final ISettingsFile testConfig = new JsonSettingsFile("testconfig.json");
+  public static final String url = testConfig.getValue("/url").toString();
+  public static final String postsEndPoint = testConfig.getValue("/postsEndPoint").toString();
+  public static final String usersEndPoint = testConfig.getValue("/usersEndPoint").toString();
+  public static final int validPostGetId = (int) testConfig.getValue("/validPostGetId");
+  public static final int validPostGetUId = (int) testConfig.getValue("/validPostGetUId");
+  public static final int invalidPostId = (int) testConfig.getValue("/invalidPostGetId");
+  public static final int postUserId = (int) testConfig.getValue("/postUserId");
+  public static final int getUserId = (int) testConfig.getValue("/getUserId");
+  public static final String userString = testConfig.getValue("/testUser").toString();
+}
